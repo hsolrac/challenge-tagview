9316441bcd69bc398d5c3e731c160f10a2c24ea8
Carlos <ch.henriquevinha@gmail>
2024-04-06 00:06:23 -0300

    Muda form para Formik com chakra-ui e adiciona validacoes

--- client/src/pages/forms/ProductForm.tsx
+++ client/src/pages/forms/ProductForm.tsx
@@ -3,62 +3,138 @@ import { createProduct } from '../../services/api'
 import { Product } from '../../types/Product'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Currency } from '../../util/Currency'
+import { Field, Form, Formik, useFormik } from 'formik';
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  Input,
+  Button,
+  Textarea,
+  Heading,
+  ButtonGroup
+} from '@chakra-ui/react'
 
-function ProductForm () {
-  const [product, setProduct] = useState<Product>({
-    nome: '',
-    descricao: '',
-    preco: 0,
-    imagem: ''
-  });
 
-  const [error, setError] = useState<string[]>([])
+function validateName(value: string) {
+  let error
+  if (!value) {
+    error = 'Nome é obrigatório'
+  } else if (value.length < 3) {
+    error = 'Nome deve ter pelo menos 3 letras'
+  }
+  return error
+}
 
-  const handleInputChange = (e: any): void => {
-    const { name, value } = e.target;
-    setProduct({ ...product, [name]: value });
-  };
+function validatePrice(value: number) {
+  let error
+  if (!value) {
+    error = 'Preço é obrigatório'
+  } else if (value < 10) {
+    error = 'Preço deve ser maior que 10'
+  }
+  return error
+}
 
-  const handleFileChange = (e: ChangeEvent<HTMLInputElement>): void => {
-    const file = e.target.files && e.target.files[0];
-    if(file){
-      setProduct({ ...product, imagem: '' });
-    }
-  };
+function validateDescription(value: string) {
+  let error
+  if (!value) {
+    error = 'Descrição é obrigatório'
+  } else if (value.length < 30) {
+    error = 'Descrição deve ter pelo menos 30 letras'
+  } else if (value.length > 255) {
+    error = 'Descrição deve ter no maximo 255 letras'
+  }
+  return error
+}
 
-  const saveProduct = async (e: FormEvent) => {
-    e.preventDefault();
-    setProduct({
-      nome: '',
-      descricao: '',
-      preco: 0,
-      imagem:'' 
-    });
-    try {
-      await createProduct(product);
-    } catch(error){
-      setError(error.message)
-    }
-  };
+function ProductForm() {
+  const [value, setValue] = useState('');
+  const format = (value: string) => Currency.format(Number(value));
 
   return (
-    <form onSubmit={saveProduct} encType="multipart/form-data">
-      <label>Nome</label>
-      <input type="text" name="nome" value={product.nome} onChange={handleInputChange} required />
-
-      <label>Descrição</label>
-      <input type="text" name="descricao" value={product.descricao} onChange={handleInputChange} required />
-
-      <label>Preço</label>
-      <input type="number" name="preco" value={product.preco} onChange={handleInputChange} required />
-
-      <label>Imagem</label>
-      <input type="file" name="imagem" onChange={handleFileChange} accept="image/*" />
-
-      <button type="submit">Salvar</button>
+    <>
+      <div style={{ margin: 'auto', width: '50%', paddingTop: '10%' }}>
+        <Heading>Cadastro de Produto</Heading>
+        <Formik
+          initialValues={{ 
+            nome: '',
+            preco: '', 
+            descricao: '', 
+            imagem: ''
+          }}
+          onSubmit={(values, actions) => {
+            createProduct(values).then(_ => 
+              actions.resetForm()
+            ).catch(error => 
+              console.error(error)
+            ).finally(() => {
+              actions.setSubmitting(false)
+            })
+          }}
+        >
+          {(props) => (
+            <Form>
+              <Field name='nome' validate={validateName}>
+                {({ field, form }: any) => (
+                  <FormControl isInvalid={form.errors.nome && form.touched.nome}>
+                    <FormLabel>Nome</FormLabel>
+                    <Input {...field} placeholder='Nome' />
+                    <FormErrorMessage>{form.errors.nome}</FormErrorMessage>
+                  </FormControl>
+                )}
+              </Field>
+              <Field name='preco' validate={validatePrice}>
+                {({ field, form }: any) => (
+                  <FormControl isInvalid={form.errors.preco && form.touched.preco}>
+                    <FormLabel>Preço</FormLabel>
+                    <Input {...field} placeholder='R$ 0,00' type="number"/>
+                    <FormErrorMessage>{form.errors.preco}</FormErrorMessage>
+                  </FormControl>
+                )}
+              </Field>
+              <Field name='descricao' validate={validateDescription}>
+                {({ field, form }: any) => (
+                  <FormControl isInvalid={form.errors.descricao && form.touched.descricao}>
+                    <FormLabel>Descrição</FormLabel>
+                    <Textarea {...field} placeholder='Descrição' />
+                    <FormErrorMessage>{form.errors.description}</FormErrorMessage>
+                  </FormControl>
+                )}
+              </Field>
+              <Field name='imagem' validate={''}>
+                {({ field, form }: any) => (
+                  <FormControl isInvalid={form.errors.imagem && form.touched.imagem}>
+                    <FormLabel>Imagem</FormLabel>
+                    <Input {...field} type='file' />
+                    <FormErrorMessage>{form.errors.imagem}</FormErrorMessage>
+                  </FormControl>
+                )}
+              </Field>
+              <ButtonGroup variant='outline' spacing='6'>
+                <Button
+                  mt={4}
+                  colorScheme='teal'
+                  isLoading={props.isSubmitting}
+                  type='submit'
+                >
+                  Salvar
+                </Button>
+                <Button
+                  as='a'
+                  href="/"
+                  mt={4}
+                >
+                  Cancelar
+                </Button>
+              </ButtonGroup>
+            </Form>
+          )}
+        </Formik>
+      </div>
       <ToastContainer />
-      {error && <div style={{ color: 'red' }}>{error}</div>}
-    </form>
+    </>
   );
 }
 
