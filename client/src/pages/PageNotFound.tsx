dc459c138e403cc233243d253142b06353689d6d
hsolrac <ch.henriquevinha@gmail.com>
2024-04-08 17:32:15 -0300

    Ajustando PageNotFound

--- client/src/pages/PageNotFound.tsx
+++ client/src/pages/PageNotFound.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { Heading, Box, Text, Button } from '@chakra-ui/react'
 
 function PageNotFound(){
+    
+  const style = {
+    display: 'flex', 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    margin: '10%'
+  }
+
   return(
-    <div>
-      <h1>Page not found</h1>
-    </div>
+    <>
+      <Box style={style}>
+        <Heading>404</Heading>
+        <Text>Pagina não encontrada</Text>
+      </Box>
+      <Box style={style}>
+        <Button as="a" href="/">Voltar para Home</Button>
+      </Box>
+    </>
   )
 }
 
